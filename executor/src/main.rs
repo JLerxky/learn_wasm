a3153ff64960ab8a09e6f82ea8497b47c27b246f
JLer <jlerxky@live.com>
2024-05-06 17:40:24 +0800

    use wasmtime

--- executor/src/main.rs
+++ executor/src/main.rs
@@ -1,41 +1,31 @@
 use tracing::error;
-use wasmer::{imports, Instance, Module, Store, Value};
+use wasi_common::sync::WasiCtxBuilder;
+use wasmtime::*;
 
-fn main() -> anyhow::Result<()> {
-    // let module_wat = r#"
-    // (module
-    //     (type $t0 (func (param i32) (result i32)))
-    //     (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
-    //         get_local $p0
-    //         i32.const 1
-    //         i32.add))
-    // "#;
-
+fn main() -> Result<()> {
     common_x::log::init_log_filter("info");
 
-    let mut store = Store::default();
-    let module = Module::from_file(&store, "plugin/pkg/plugin_bg.wasm").map_err(|e| {
-        error!("0: {e}");
-        e
-    })?;
-    // The module doesn't import anything, so we create an empty import object.
-    let import_object = imports! {};
-    let instance = Instance::new(&mut store, &module, &import_object).map_err(|e| {
-        error!("1: {e}");
-        e
-    })?;
+    let engine = Engine::default();
+    let mut linker = Linker::new(&engine);
+    wasi_common::sync::add_to_linker(&mut linker, |s| s)?;
 
-    let add_one = instance.exports.get_function("add").map_err(|e| {
-        error!("2: {e}");
-        e
-    })?;
-    let result = add_one
-        .call(&mut store, &[Value::I32(1), Value::I32(1)])
-        .map_err(|e| {
-            error!("3: {e}");
+    let wasi = WasiCtxBuilder::new()
+        .inherit_stdio()
+        .inherit_args()?
+        .build();
+    let mut store = Store::new(&engine, wasi);
+
+    let module =
+        Module::from_file(&engine, "target/wasm32-wasi/release/hello.wasm").map_err(|e| {
+            error!("failed to load module: {}", e);
             e
         })?;
-    assert_eq!(result[0], Value::I32(2));
+    linker.module(&mut store, "", &module)?;
+
+    let instantiate = linker.instantiate(&mut store, &module)?;
+    instantiate
+        .get_typed_func::<(), ()>(&mut store, "_start")?
+        .call(&mut store, ())?;
 
     Ok(())
 }
---
+++ plugin/src/hello.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("hello from main");
+    hello();
+}
+
+pub fn hello() {
+    println!("hello from hello");
+}
